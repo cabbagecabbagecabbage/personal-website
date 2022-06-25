e4d4ba989864a73c07bcd0403d7eb8eadf016a05
cabbagecabbagecabbage <48808428+cabbagecabbagecabbage@users.noreply.github.com>
2022-06-25 11:47:33 -0400

    kinda responsive?

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const mainNavLinks = document.getElementsByClassName('main-nav-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    mainNavLinks.classList.toggle('active');
+    console.log("hi");
+});
