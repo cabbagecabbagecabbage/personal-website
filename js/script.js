925e92b8c9c47fae4562a0dc7e82d5daecddd1ea
cabbagecabbagecabbage <48808428+cabbagecabbagecabbage@users.noreply.github.com>
2022-06-27 18:07:32 -0400

    udpated js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const mainNavLinks = document.getElementsByClassName('main-nav-links')[0];
+$(document).ready(function(){
+    $(function(){
+        console.log("ok");
+        $("#header").load("header.html", function() {
+            const toggleButton = document.getElementsByClassName('toggle-button')[0];
+            const mainNavLinks = document.getElementsByClassName('main-nav-links')[0];
 
-toggleButton.addEventListener('click', () => {
-    mainNavLinks.classList.toggle('active');
+            toggleButton.addEventListener('click', () => {
+                mainNavLinks.classList.toggle('active');
+            });
+        }); 
+        $("#footer").load("footer.html"); 
+    });
 });
+
+
